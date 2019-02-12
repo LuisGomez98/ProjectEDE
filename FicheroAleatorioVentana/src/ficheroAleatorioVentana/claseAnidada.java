464d459ddf9af0ab8d061022e5d0fbb812d6b1f6
1dam <1dam@HZ301196.TartangaLH.eus>
2019-02-12 13:28:36 +0100

    primer cambio

---
+++ FicheroAleatorioVentana/src/ficheroAleatorioVentana/claseAnidada.java
@@ -0,0 +1,13 @@
+package ficheroAleatorioVentana;
+
+class claseAnidada {	
+	void entrada()
+	{
+		System.out.println("Método entrada.");
+	}
+	String salida (int d)
+	{
+		System.out.println("Salida.");
+		return "Salida el "+d;
+	}
+}
